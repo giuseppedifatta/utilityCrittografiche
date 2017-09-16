4d2a353f57e221d739ad05a7a0801cbb2c702746
Giuseppe Di Fatta <giuseppe.difatta89@gmail.com>
2017-09-16 13:17:24 +0200

    a

--- testCBC_AES/src/Driver.cpp
+++ testCBC_AES/src/Driver.cpp
@@ -3,8 +3,8 @@
 
 #include "cryptopp/osrng.h"
 
-
 #include <iostream>
+#include <chrono>
 using std::cout;
 using std::cerr;
 using std::endl;
@@ -28,6 +28,7 @@ using std::exit;
 
 
 #include "cryptopp/ccm.h"
+#include "cryptopp/rsa.h"
 
 
 
@@ -38,52 +39,78 @@ using namespace CryptoPP;
 int main(int argc, char* argv[])
 {
 	//generazione chiave simmetrica e iv
-	AutoSeededRandomPool rng;
-
-	// chiave simmetrica
-	SecByteBlock key( AES::MAX_KEYLENGTH ); //AES::MAX_KEYLENGTH=32
-	rng.GenerateBlock( key, key.size() );
-	//cout << key.size() << " !=  "<< sizeof(key) << endl;
-	// Pretty print key
-	string encoded;
-	encoded.clear();
-	StringSource(key, key.size(), true,
-			new HexEncoder(
-					new StringSink(encoded)
-			) // HexEncoder
-	); // StringSource
-	cout << "key: " << encoded << endl;
-
-	//initial value
-	SecByteBlock iv(AES::BLOCKSIZE);
-	rng.GenerateBlock( iv, iv.size() );
-	// Pretty print iv
-	encoded.clear();
-	StringSource(iv,iv.size(),true,
-			new HexEncoder(
-					new StringSink(encoded)
-			) // HexEncoder
-	); // StringSource
-	cout << "iv: " << encoded << endl;
-
-	//	string encodedIV;
-	//	byte iv[AES::BLOCKSIZE];
-	//	memset(iv, 0x01,AES::BLOCKSIZE);
-	//	std::string s_iv( reinterpret_cast< char const* >(iv) ) ;
-	//	StringSource(s_iv, true,
+	//	AutoSeededRandomPool rng;
+	//
+	//	// chiave simmetrica
+	//	SecByteBlock key( AES::MAX_KEYLENGTH ); //AES::MAX_KEYLENGTH=32
+	//	rng.GenerateBlock( key, key.size() );
+	//	//cout << key.size() << " !=  "<< sizeof(key) << endl;
+	//	// Pretty print key
+	//	string encoded;
+	//	encoded.clear();
+	//	StringSource(key, key.size(), true,
 	//			new HexEncoder(
-	//					new StringSink(encodedIV)
+	//					new StringSink(encoded)
 	//			) // HexEncoder
 	//	); // StringSource
-	//	cout << "iv: " << encodedIV << endl;
+	//	cout << "key: " << encoded << endl;
+	//
+	//	//initial value
+	//	SecByteBlock iv(AES::BLOCKSIZE);
+	//	rng.GenerateBlock( iv, iv.size() );
+	//	// Pretty print iv
+	//	encoded.clear();
+	//	StringSource(iv,iv.size(),true,
+	//			new HexEncoder(
+	//					new StringSink(encoded)
+	//			) // HexEncoder
+	//	); // StringSource
+	//	cout << "iv: " << encoded << endl;
+	string encodedKey = "BB2385F128C3B2B1E42C763D2BBA8553E0776666B2DEDE7BCFBDFA9AAB97EF68";
+	string k;
+	StringSource(encodedKey,true, new HexDecoder(new StringSink(k)));
+	SecByteBlock key(reinterpret_cast< const byte*>(k.data()),k.size());
+	string encodedIV;
+	byte iv[AES::BLOCKSIZE];
+	memset(iv, 0x01,AES::BLOCKSIZE);
+	std::string s_iv( reinterpret_cast< char const* >(iv) ) ;
+	StringSource(s_iv, true,
+			new HexEncoder(
+					new StringSink(encodedIV)
+			) // HexEncoder
+	); // StringSource
+	cout << "iv: " << encodedIV << endl;
 
-	string plain = "CBC Mode Test";
+	///////////////////// //genero il mio plain
+
+	//string plain = "CBC Mode Test";
+	AutoSeededRandomPool rng;
+
+	RSA::PrivateKey rsaPrivate;
+	rsaPrivate.GenerateRandomWithKeySize(rng, 3072);
+
+	//RSA::PublicKey rsaPublic(rsaPrivate);
+
+	ByteQueue queue2;
+	rsaPrivate.Save(queue2);
+
+	//copiamo la privateKey dal ByteQueue in una stringa
+	string privateKey;
+	StringSink ss2(privateKey);
+	queue2.CopyTo(ss2);
+	ss2.MessageEnd();
+	cout << "privateKey: " << privateKey << endl; //formato byte
+
+	string encodedPR;
+	StringSource(privateKey, true,
+			new HexEncoder(new StringSink(encodedPR)));
+	cout << "encodedPrivateKey: " << encodedPR << endl;
+	string plain = privateKey;
 	string cipher, recovered;
+	//////////////////////////
 
 
-
-	/*********************************\
-	\*********************************/
+	//auto t0 = std::chrono::high_resolution_clock::now();
 
 	try
 	{
@@ -107,7 +134,10 @@ int main(int argc, char* argv[])
 		cerr << e.what() << endl;
 		exit(1);
 	}
+	//auto t1 = std::chrono::high_resolution_clock::now();
+	//auto dt = 1.e-9*std::chrono::duration_cast<std::chrono::nanoseconds>(t1-t0).count();
 
+	//cout << "Time to encrypt privateKey: " << dt << " second(s)"  << endl;
 
 
 
@@ -115,6 +145,7 @@ int main(int argc, char* argv[])
 	\*********************************/
 
 	// Pretty print
+	string encoded;
 	encoded.clear();
 	StringSource(cipher, true,
 			new HexEncoder(
@@ -125,6 +156,7 @@ int main(int argc, char* argv[])
 
 	/*********************************\
 	\*********************************/
+
 
 	try
 	{
@@ -150,9 +182,17 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
+
 	/*********************************\
 	\*********************************/
 
+	if (plain == recovered){
+		cout << "recovered plain" << endl;
+	}
+	string decodedPrivateKey;
+	StringSource(recovered, true,
+			new HexDecoder(new StringSink(decodedPrivateKey)));
+	//cout << "decodedPRivateKey" << decodedPrivateKey << endl;
 	return 0;
 }
 
